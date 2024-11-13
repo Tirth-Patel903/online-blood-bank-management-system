442b378674143ae44e9caa7877f0f392f9bd7411
Tirth Patel <99315530+Tirth-Patel903@users.noreply.github.com>
2024-11-13 10:35:17 +0530

    Add files via upload

---
+++ users/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="nav navbar-fixed-bottom">
+            <b>Made By: <a href="http://sandeep&ruchi.com">sandeep&ruchi</a></b>
+            <br>
+            &COPY;AIMIS,Project
+</footer>
+
+
+
+</body>
+</html>
